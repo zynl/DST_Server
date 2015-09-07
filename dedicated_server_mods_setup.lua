aaef0a44a4770c042d377560badeb26bb1a1cf8c
zynl <1917386433@qq.com>
2015-09-07 11:49:33 +0800

    Update dedicated_server_mods_setup.lua

--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -1,15 +1,15 @@
-ForceEnableMod("workshop-345692228")  --Minimap HUD
-ForceEnableMod("workshop-409184357")  --Zoro
-ForceEnableMod("workshop-376333686")  --Always On Status 
-ForceEnableMod("workshop-373991022")  --Global Player Icons
-ForceEnableMod("workshop-375859599")  --Health Info
---ForceEnableMod("workshop-374550642")  --Increased Stack size
---ForceEnableMod("workshop-378160973")  --Global Positions
-ForceEnableMod("workshop-347079953")    --Display Food Values
-ForceEnableMod("workshop-382177939")    --DST Storm Cellar
---ForceEnableMod("workshop-450498063")  --Illyasviel Von Einzbern(Illya)
-ForceEnableMod("workshop-375850593")    --Extra Equip Slots
-ForceEnableMod("workshop-362175979")    --Wormhole Marks [DST]
-ForceEnableMod("workshop-367546858")    --EZInput
-ForceEnableMod("workshop-439115156")    --[DST] Musha [5.8 F]
-ForceEnableMod("workshop-364491382")    --Faroz Character [DST]
+ServerModSetup("345692228") --Minimap HUD
+ServerModSetup("409184357") --Zoro
+ServerModSetup("376333686") --Always On Status
+ServerModSetup("373991022") --Global Player Icons
+ServerModSetup("375859599") --Health Info
+--ServerModSetup("374550642") --Increased Stack size
+--ServerModSetup("378160973") --Global Positions
+ServerModSetup("347079953") --Display Food Values
+ServerModSetup("382177939") --DST Storm Cellar
+--ServerModSetup("450498063") --Illyasviel Von Einzbern(Illya)
+ServerModSetup("375850593") --Extra Equip Slots
+ServerModSetup("362175979") --Wormhole Marks [DST]
+ServerModSetup("367546858") --EZInput
+ServerModSetup("439115156") --[DST] Musha [5.8 F]
+ServerModSetup("364491382") --Faroz Character [DST]
