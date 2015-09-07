52e48a25b3352dd44421a9ade2de054498e1b958
zynl <1917386433@qq.com>
2015-09-07 10:25:03 +0800

    debian 7.7

--- start_dst.sh
+++ start_dst.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/steam/steamapps/DST/bin
-/usr/bin/screen -S "DST" /bin/sh -c './dontstarve_dedicated_server_nullrenderer -console'
+/usr/bin/screen -S "DST" bash -c 'LD_LIBRARY_PATH=~/dst_lib ./dontstarve_dedicated_server_nullrenderer -console'
