4dfb624acda07d63765ed118ae609bd4b910202e
zynl <1917386433@qq.com>
2015-09-13 02:14:16 +0800

    Update update_dst.sh

--- update_dst.sh
+++ update_dst.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-/home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/steam/steamapps/DST +app_update 343050 validate +quit
-/home/steam/steamapps/DST/bin/dontstarve_dedicated_server_nullrenderer -only_update_server_mods
+
+echo "Killing DST session(s) ..."
+screen -ls | grep DST | cut -d. -f1 | awk '{print $1}' | xargs kill
+echo "Session killed, will now update DST..."
+cd /home/steam/steamcmd/ && ./steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/steam/steamapps/DST +app_update 343050 validate +quit
+echo "Update completed, will now start the server..."
+su - steam
+'~/start_dst.sh;
+exit'
+echo "Server successfully started!"
