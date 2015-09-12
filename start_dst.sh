0425445a88f0c0a6cf69da3dc2c1e9fd835f30cf
zynl <1917386433@qq.com>
2015-09-13 01:48:31 +0800

    Update start_dst.sh

--- start_dst.sh
+++ start_dst.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/steam/steamapps/DST/bin
-/usr/bin/screen -S "DST" bash -c 'LD_LIBRARY_PATH=~/dst_lib ./dontstarve_dedicated_server_nullrenderer -console'
+screen -S "DST Server" ./dontstarve_dedicated_server_nullrenderer
+#/usr/bin/screen -S "DST" bash -c 'LD_LIBRARY_PATH=~/dst_lib ./dontstarve_dedicated_server_nullrenderer -console'
