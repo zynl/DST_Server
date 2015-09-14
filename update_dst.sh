71d9597ee84c3718ec8c6c2593346f7b6f26bb0f
zynl <1917386433@qq.com>
2015-09-14 15:43:31 +0800

    Update update_dst.sh

--- update_dst.sh
+++ update_dst.sh
@@ -3,9 +3,7 @@
 echo "Killing DST session(s) ..."
 screen -ls | grep DST | cut -d. -f1 | awk '{print $1}' | xargs kill
 echo "Session killed, will now update DST..."
-cd /home/steam/steamcmd/ && ./steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/steam/steamapps/DST +app_update 343050 validate +quit
+cd ~/steamcmd/ && ./steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/steam/steamapps/DST +app_update 343050 validate +quit
 echo "Update completed, will now start the server..."
-su - steam
-'~/start_dst.sh;
-exit'
+~/DST_Server/start_dst.sh
 echo "Server successfully started!"
