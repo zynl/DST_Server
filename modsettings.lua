2d8dcddfdbbb4d31d6868e56f2c9300ef88187b5
zynl <1917386433@qq.com>
2015-09-15 17:28:50 +0800

    Update modsettings.lua

--- modsettings.lua
+++ modsettings.lua
@@ -1,14 +1,26 @@
+-- Use the "ForceEnableMod" function when developing a mod. This will cause the
+-- game to load the mod every time no matter what, saving you the trouble of
+-- re-enabling it from the main menu.
+--
+-- Note! You shout NOT do this for normal mod loading. Please use the Mods menu
+-- from the main screen instead.
+
+-- ForceEnableMod("kioskmode_dst")
+
+
+-- Use "EnableModDebugPrint()" to show extra information during startup.
+
 EnableModDebugPrint()
 ForceEnableMod("workshop-345692228")  --Minimap HUD
-ForceEnableMod("workshop-409184357")  --Zoro
-ForceEnableMod("workshop-376333686")  --Always On Status 
-ForceEnableMod("workshop-373991022")  --Global Player Icons
+--ForceEnableMod("workshop-409184357")  --Zoro
+ForceEnableMod("workshop-376333686")  --Always On Status
+--ForceEnableMod("workshop-373991022")  --Global Player Icons
 ForceEnableMod("workshop-375859599")  --Health Info
 --ForceEnableMod("workshop-374550642")  --Increased Stack size
 --ForceEnableMod("workshop-378160973")  --Global Positions
-ForceEnableMod("workshop-347079953")    --Display Food Values
+--ForceEnableMod("workshop-347079953")    --Display Food Values
 ForceEnableMod("workshop-382177939")    --DST Storm Cellar
---ForceEnableMod("workshop-450498063")  --Illyasviel Von Einzbern(Illya)
+ForceEnableMod("workshop-450498063")  --Illyasviel Von Einzbern(Illya)
 ForceEnableMod("workshop-375850593")    --Extra Equip Slots
 ForceEnableMod("workshop-362175979")    --Wormhole Marks [DST]
 ForceEnableMod("workshop-367546858")    --EZInput
