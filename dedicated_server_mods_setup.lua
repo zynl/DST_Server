db26e51295ab6d7566eacbb3d0b23b8b2c4a005c
zynl <1917386433@qq.com>
2015-09-15 19:49:12 +0800

    Update dedicated_server_mods_setup.lua

--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -3,11 +3,11 @@ ServerModSetup("409184357") --Zoro
 ServerModSetup("376333686") --Always On Status
 ServerModSetup("373991022") --Global Player Icons
 ServerModSetup("375859599") --Health Info
---ServerModSetup("374550642") --Increased Stack size
---ServerModSetup("378160973") --Global Positions
+ServerModSetup("374550642") --Increased Stack size
+ServerModSetup("378160973") --Global Positions
 ServerModSetup("347079953") --Display Food Values
 ServerModSetup("382177939") --DST Storm Cellar
---ServerModSetup("450498063") --Illyasviel Von Einzbern(Illya)
+ServerModSetup("450498063") --Illyasviel Von Einzbern(Illya)
 ServerModSetup("375850593") --Extra Equip Slots
 ServerModSetup("362175979") --Wormhole Marks [DST]
 ServerModSetup("367546858") --EZInput
