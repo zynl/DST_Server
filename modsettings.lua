c2fd190171686990666da01d1e2bb5c1306b9c95
zynl <1917386433@qq.com>
2015-09-20 00:57:54 +0800

    Update modsettings.lua

--- modsettings.lua
+++ modsettings.lua
@@ -11,20 +11,21 @@
 -- Use "EnableModDebugPrint()" to show extra information during startup.
 
 EnableModDebugPrint()
-ForceEnableMod("workshop-345692228")  --Minimap HUD
+--ForceEnableMod("workshop-345692228")  --Minimap HUD
 --ForceEnableMod("workshop-409184357")  --Zoro
-ForceEnableMod("workshop-376333686")  --Always On Status
+--ForceEnableMod("workshop-376333686")  --Always On Status
 --ForceEnableMod("workshop-373991022")  --Global Player Icons
 ForceEnableMod("workshop-375859599")  --Health Info
 --ForceEnableMod("workshop-374550642")  --Increased Stack size
---ForceEnableMod("workshop-378160973")  --Global Positions
+ForceEnableMod("workshop-378160973")  --Global Positions
 --ForceEnableMod("workshop-347079953")    --Display Food Values
 ForceEnableMod("workshop-382177939")    --DST Storm Cellar
-ForceEnableMod("workshop-450498063")  --Illyasviel Von Einzbern(Illya)
-ForceEnableMod("workshop-375850593")    --Extra Equip Slots
-ForceEnableMod("workshop-362175979")    --Wormhole Marks [DST]
-ForceEnableMod("workshop-367546858")    --EZInput
+--ForceEnableMod("workshop-450498063")  --Illyasviel Von Einzbern(Illya)
+--ForceEnableMod("workshop-375850593")    --Extra Equip Slots
+--ForceEnableMod("workshop-362175979")    --Wormhole Marks [DST]
+--ForceEnableMod("workshop-367546858")    --EZInput
 ForceEnableMod("workshop-439115156")    --[DST] Musha [5.8 F]
-ForceEnableMod("workshop-364491382")    --Faroz Character [DST]
+--ForceEnableMod("workshop-364491382")    --Faroz Character [DST]
 ForceEnableMod("workshop-451559807")    --cave mod
-ForceEnableMod("workshop-351325790")    --Geometric Placement
+--ForceEnableMod("workshop-351325790")    --Geometric Placement
+ForceEnableMod("workshop-363989569")      --Growable Marble Trees
