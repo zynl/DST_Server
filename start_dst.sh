93f16a4bd4618a39ff13ea53196918cfbc4e39ea
zynl <1917386433@qq.com>
2015-09-21 20:19:45 +0800

    Update start_dst.sh

--- start_dst.sh
+++ start_dst.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/steam/steamapps/DST/bin
-screen -S "DST Server" ./dontstarve_dedicated_server_nullrenderer
+
+screen -dmS "DST Server" sh -c "cd /home/steam/steamapps/DST/bin; ./dontstarve_dedicated_server_nullrenderer"
+screen -dmS "DST_caves Server" sh -c "cd /home/steam/steamapps/DST/bin; ./dontstarve_dedicated_server_nullrenderer -conf_dir DST_Cave"
 #/usr/bin/screen -S "DST" bash -c 'LD_LIBRARY_PATH=~/dst_lib ./dontstarve_dedicated_server_nullrenderer -console'
